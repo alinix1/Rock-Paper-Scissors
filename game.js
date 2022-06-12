85eeac2e0a620dced51ae9f0adaef93ec5c9888f
Ali <anix1@kent.edu>
2022-06-12 12:23:11 -0600

    Add functionality to the game and refactor

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ class Game {
     this.human = human;
     this.computer = computer;
     this.isClassicGameType = true;
-    
+
   }
   setClassicGameType() {
     this.isClassicGameType = true;
@@ -11,49 +11,36 @@ class Game {
   setDifficultGameType() {
     this.isClassicGameType = false;
   }
-  getPossibleMoves() {
-    if (this.isClassicGameType) {
-      return ['rock', 'paper', 'scissors'];
-    } else {
-      return ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-    }
-  }
-  // handlePlayerMove(playerSelection) {
-  //   var computerSelection = this.computer.takeTurn(this.getPossibleMoves())
-  //   if (playerSelection === computerSelection) {
-  //     return 'TIE';
-  //   }
-  //   var playerWins = false
-  //   if (this.isClassicGameType) {
-  //     playerWins = checkIfPlayerWinsClassic(playerSelection, computerSelection);
-  //   } else {
-  //     playerWins = checkIfPlayerWinsDifficult(playerSelection, computerSelection)
-  //   }
-  //   if (playerWins) {
-  //       // increment human.wins
-  //       // return 'HUMAN'
-  //   } else {
-  //       // increment computer.wins
-  //       // return 'COMPUTER'
-  //   }
   checkIfPlayerWinsClassic() {
     if (this.human.token === 'rock' && this.computer.token === 'scissors' || this.human.token === 'paper' && this.computer.token === 'rock' || this.human.token === 'scissors' && this.computer.token === 'paper') {
       this.human.increaseWins();
-      return subHeaderText.innerText = "Human won this round!";
+      return true;
     } else if (this.human.token === 'rock' && this.computer.token === 'paper' || this.human.token === 'paper' && this.computer.token === 'scissors' || this.human.token === 'scissors' && this.computer.token === 'rock') {
       this.computer.increaseWins();
-      return subHeaderText.innerText = "Computer won this round!";
+      return false;
     } else if (this.human.token === this.computer.token) {
-      return subHeaderText.innerText = "It's a draw!";
+      return undefined;
     }
   }
   checkIfPlayerWinsDifficult() {
     if (this.human.token === 'rock' && this.computer.token === 'scissors' || this.human.token === 'paper' && this.computer.token === 'rock' || this.human.token === 'scissors' && this.computer.token === 'paper' || this.human.token === 'rock' && this.computer.token === 'lizard' || this.human.token === 'paper' && this.computer.token === 'spock' || this.human.token === 'scissors' && this.computer.token === 'lizard' || this.human.token === 'lizard' && this.computer.token === 'paper' || this.human.token === 'lizard' && this.computer.token === 'spock' || this.human.token === 'spock' && this.computer.token === 'scissors' || this.human.token === 'spock' && this.computer.token === 'rock') {
       this.human.increaseWins();
-      return subHeaderText.innerText = 'Human won this round!';
+      return true;
     } else if (this.human.token === 'rock' && this.computer.token === 'paper' || this.human.token === 'paper' && this.computer.token === 'scissors' || this.human.token === 'scissors' && this.computer.token === 'rock' || this.human.token === 'rock' && this.computer.token === 'spock' || this.human.token === 'paper' && this.computer.token === 'lizard' || this.human.token === 'scissors' && this.computer.token === 'spock' || this.human.token === 'lizard' && this.computer.token === 'rock' || this.human.token === 'lizard' && this.computer.token === 'scissors' || this.human.token === 'spock' && this.computer.token === 'paper' || this.human.token === 'spock' && this.computer.token === 'lizard') {
       this.computer.increaseWins();
-      return subHeaderText.innerText = 'Computer won this round!';
+      return false;
+    } else {
+      return undefined;
+    }
+  }
+  resetGame() {
+    chosenPlayers.classList.add('hidden');
+    changeGameButton.classList.remove('hidden');
+    subHeaderText.innerText === 'Choose your fighter!';
+    if (this.isClassicGameType) {
+      showClassicOptions();
+    } else {
+      showDifficultOptions();
     }
   }
 };
